f87861022fb1b4d7956f2d1f4cf39e3bd8d68460
Amir Mahdi Tabatabaei <seyedamirmahdi313yaran@gmail.com>
2026-08-16 21:20:06 +0330

    refactor: dataprep has optional test data return

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,20 +1,20 @@
 """
-This python file's for preproccesing and data Scrubbing
+This python file's for preprocessing and data Scrubbing
 """
 
-# import  moduls
+#===========import  moduls==============
 import os
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-
-def loader(show_print=False):
-    # load dataset from DataSet folder in csv format
+#=============functions=================
+def loader(show_print=False,return_test=False):
+    #==============read CSV=============
     credits_df = pd.read_csv(r"E:\MLprojects\maktap-project-1\DataSet\creditcard.csv")
 
-    # analyzing dataset
+    #=======analyzing dataset========
     if show_print:
         print(credits_df.head(3))
         print("_" * 20)
@@ -22,7 +22,7 @@ def loader(show_print=False):
         print("_" * 20)
         print(credits_df.describe())
 
-        # check for nan
+        #==========number of Nan and Duplicates=========
         print("_" * 20)
         print(f"Sum of nan \n{credits_df.isna().sum()}")
         print("_" * 20)
@@ -31,7 +31,7 @@ def loader(show_print=False):
         print(credits_df["Class"].value_counts(normalize=True))
     credits_df.drop_duplicates(inplace=True)
 
-    # split data to train and test
+    #==========split==============
     Xtrain, Xtest, ytrain, ytest = train_test_split(
         credits_df.iloc[:, :-1].to_numpy(),
         credits_df.iloc[:, -1].to_numpy(),
@@ -39,10 +39,12 @@ def loader(show_print=False):
         random_state=42,
         stratify=credits_df.iloc[:, -1],
     )
+    #============Extract Validation==============
     Xtrain, Xval, ytrain, yval = train_test_split(
         Xtrain, ytrain, test_size=0.2, random_state=42, stratify=ytrain
     )
 
+    #==========scaler==============
     scaler = StandardScaler()
     scaler.fit(Xtrain)
 
@@ -54,22 +56,25 @@ def loader(show_print=False):
              ytrain {ytrain.shape}, \
               yval {yval.shape},\
              ytest {ytest.shape}")
-    # save scaler for next uses
-    if not os.path.exists(r"E:\MLprojects\maktap-project-1\models"):
-        os.makedirs(r"E:\MLprojects\maktap-project-1\models", exist_ok=True)
-    # save scaler object in models folder
+
+    #===============Save-Scaler==============
+    os.makedirs(r"E:\MLprojects\maktap-project-1\models", exist_ok=True)
     joblib.dump(scaler, r"E:\MLprojects\maktap-project-1\models\scaler.pkl")
 
-    return (
-        Xtrain,
-        Xval,
-        Xtest,
-        ytrain,
-        yval,
-        ytest,
-    )
+    #==========Optional Return===============
+    if return_test:
+        return Xtest,ytest
+    else:
+        return (
+            Xtrain,
+            Xval,
+            ytrain,
+            yval,
+
+        )
 
 
+#===============Test Unit=================
 if __name__ == "__main__":
-    Xtrain, Xval, Xtest, ytrain, yval, ytest = loader(show_print=False)
+    Xtrain, Xval, ytrain, yval = loader(show_print=False)
     print("Data processed successfully!")
