4d0946a35bf0baad2bd3ce686892d07ff96104d3
Amir Mahdi Tabatabaei <seyedamirmahdi313yaran@gmail.com>
2026-08-17 13:19:30 +0330

    refactor: not scaled refactore for better style and better performance;also path or not absolute any more

--- src/train/not_scaled_knn.py
+++ src/train/not_scaled_knn.py
@@ -1,14 +1,15 @@
 """This file trains four model KNN"""
 
-# imports
+#================imports=================
 import matplotlib.pyplot as plt
 import joblib
-import os
+from pathlib import Path
 from src import data_prep
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 
-# metrics
+#================metrics=================
+
 from sklearn.metrics import (
     precision_score,
     recall_score,
@@ -19,8 +20,10 @@ from sklearn.metrics import (
 )
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
 
+base_dir = Path(__file__).resolve().parent.parent.parent
 
-# functions
+#================trainer=================
+
 def knn_fitter(Xtrain, Xtest, ytrain, n_neighbors: list):
     """This function trains KNN classifier."""
     knn = KNeighborsClassifier()
@@ -38,33 +41,33 @@ def knn_fitter(Xtrain, Xtest, ytrain, n_neighbors: list):
 
 
 # ============================================================
-# load data from data_prep.py
+#                 load data from data_prep.py
 # ============================================================
-# not scaled data
 Xtrain, Xval, Xtest_final, ytrain, yval, ytest_final = data_prep.loader(
     show_print=False
 )
 
 
-# We will not scale dataset this time to see not scaled data how affect KNN
-
+# ================================================
+#           This time we don't need scaler
+# ================================================
 
 # ================================================
-# KNN training | scaled data
+#             KNN training | scaled data
 # ================================================
 thresholds = [0.3, 0.5, 0.7]
 proba_knn, proba_train_knn, knn = knn_fitter(
     Xtrain, Xval, ytrain, n_neighbors=[1, 5, 20]
 )
 
-results_path = r"E:\MLprojects\maktap-project-1\reports\knn_not_scaled"
-os.makedirs(results_path, exist_ok=True)
+results_path = base_dir / "reports" / "knn_not_scaled"
+results_path.mkdir(parents=True, exist_ok=True)
 
 for threshold in thresholds:
     ypred_knn = proba_knn >= threshold
     ypred_train_knn = proba_train_knn >= threshold
 
-    # confusion matrix train
+    # ================Confusion matrix=================
     cm_train = confusion_matrix(ytrain, ypred_train_knn)
     dsp = ConfusionMatrixDisplay(
         confusion_matrix=cm_train, display_labels=["legitimate", "fraud"]
@@ -74,13 +77,13 @@ for threshold in thresholds:
         f"KNN train cm K = {knn.best_params_['n_neighbors']} threshold: {threshold}"
     )
 
-    # save plot
-
+    # ================Saveplot=================
     plt.savefig(
-        f"E:\\MLprojects\\maktap-project-1\\reports\\knn_not_scaled\\knn_cm_train{int(threshold*10)}.png"
+        results_path / f"knn_cm_train{int(threshold*10)}.png"
     )
     plt.close()
-    # ================================= test
+
+    # ================Save reports=================
 
     # confusion matrix test
     cm = confusion_matrix(yval, ypred_knn)
@@ -90,14 +93,14 @@ for threshold in thresholds:
     dsp.plot()
     plt.title(f"KNN cm K = {knn.best_params_['n_neighbors']} threshold {threshold}")
 
-    # save plot test
+    # ================Save-reports=================
     plt.savefig(
-        f"E:\\MLprojects\\maktap-project-1\\reports\\knn_not_scaled\\knn_cm{int(threshold*10)}.png"
+        results_path / f"knn_cm{int(threshold*10)}.png"
     )
     plt.close()
 
     # ================================================
-    # Save train and validation scores to CSV
+    #    Save train and validation scores to CSV
     # ================================================
     results = pd.DataFrame(
         [
@@ -123,14 +126,14 @@ for threshold in thresholds:
     )
 
     results.to_csv(
-        os.path.join(results_path, f"knn_scores{int(threshold*10)}.csv"),
+        results_path / f"knn_scores{int(threshold*10)}.csv",
         index=False,
     )
 
     print("\nKNN scores saved successfully.")
 
 
-# save model
-model_path = r"E:\MLprojects\maktap-project-1\models"
-os.makedirs(model_path, exist_ok=True)
-joblib.dump(knn.best_estimator_, os.path.join(model_path, f"knn_not_scaled.pkl"))
+#================save model=================
+model_path = base_dir / "models"
+model_path.mkdir(parents=True, exist_ok=True)
+joblib.dump(knn.best_estimator_, model_path / "knn_not_scaled.pkl")
